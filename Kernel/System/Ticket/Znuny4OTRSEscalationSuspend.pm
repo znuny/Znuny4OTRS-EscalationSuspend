1674ce1018424a94e0e08668d5f9f95c8846aac6
Denny Bresch <db@znuny.com>
2018-11-16 14:49:00 +0100

    Applied CodePolicy.

--- Kernel/System/Ticket/Znuny4OTRSEscalationSuspend.pm
+++ Kernel/System/Ticket/Znuny4OTRSEscalationSuspend.pm
@@ -1,12 +1,12 @@
 # --
-# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
-# Copyright (C) 2012-2017 Znuny GmbH, http://znuny.com/
+# Copyright (C) 2001-2018 OTRS AG, https://otrs.com/
+# Copyright (C) 2012-2018 Znuny GmbH, http://znuny.com/
 # --
 # $origin: otrs - e58542a3302a50cf0eac29b63c80206f1de2608e - Kernel/System/Ticket.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
 # --
 # ---
 # Znuny4OTRS-EscalationSuspend
